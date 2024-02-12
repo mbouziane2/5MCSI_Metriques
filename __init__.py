3fb3499bab5cdd1a07fe0f4b7b9d97c3f174ac3a
mbouziane2 <159757441+mbouziane2@users.noreply.github.com>
2024-02-12 10:19:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,20 +38,9 @@ def mongraphique2():
 
 repo_url = "https://api.github.com/repos/mbouziane2/5MCSI_Metriques/commits"
 
-@app.route('/commits/')
-def commits():
-    response = requests.get(repo_url)
-    commits_data = response.json()
-
-    commit_minutes = [extract_minutes(commit['commit']['author']['date']) for commit in commits_data]
-
-    return render_template('commits.html', commit_minutes=commit_minutes)
-
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return jsonify({'minutes': minutes})
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
